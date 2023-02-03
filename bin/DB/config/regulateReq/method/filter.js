7b16c64a80c3f67c529b3ffee971934b7e417cbf
ecomkelin <ecomkelin@gmail.com>
2023-02-03 15:51:02 +0100

    1.3.0

--- bin/DB/config/regulateReq/method/filter.js
+++ bin/DB/config/regulateReq/method/filter.js
@@ -4,27 +4,37 @@
 const getCLfield = require("../func/getCLfield");
 
 module.exports = (ctxObj, MToptions) => {
-    const {filter = {}} = ctxObj.reqBody;
-    const { payload } = ctxObj.Koptions;
+    const {reqBody, Koptions} = ctxObj;
+    const { payload } = Koptions;
 
-    const matchObj = {};
 
-    const { CLdoc } = MToptions;
-
+    const match = {};
     /** 分销模式 区分 Firm 用的 */
     let Firm = null;
     if (payload.Firm) Firm = payload.Firm;
-    if (Firm) matchObj.Firm = Firm;
+    if (Firm) match.Firm = Firm;
 
-    let { _id, search, match = {}, includes = {}, excludes = {}, lte = {}, gte = {}, at_before = {}, at_after = {} } = filter;
+    if(reqBody.match) {
+        reqBody.match = {...reqBody.match, ...match};
+    } else{
+        reqBody.match = match;
+        filterParse(reqBody.match, reqBody.filter, MToptions.CLdoc)
+    }
 
+    
+    
+}
+
+
+const filterParse = (match, filter, CLdoc) => {
+    let { _id, search, exact = {}, includes = {}, excludes = {}, lte = {}, gte = {}, at_before = {}, at_after = {} } = filter;
     if (_id) {   // 一定是 ***One 方法
         if (!isObjectIdAbs(_id)) return { errMsg: "filter._id 必须为 ObjectId 类型" };
-        matchObj._id = _id;
+        match._id = _id;
     }
 
     if (search) {
-        let matchOr = [];
+        let exactOr = [];
         /** 参数 fields 需要模糊查询的字段（可以是 数组）
          * keywords 需要查询的 字符串
          */
@@ -42,16 +52,16 @@ module.exports = (ctxObj, MToptions) => {
                 if (docField.errMsg) return { errMsg: '[filter.search.fields]: ' + docField.errMsg };
 
                 if (docField.type !== String) return { errMsg: `数据层readPre [filter.search.fields 的值 ${field}] 错误, 应该传递类型为<String>的<field>` };
-                matchOr.push({ [field]: { $regex: keywords, $options: 'i' } });
+                exactOr.push({ [field]: { $regex: keywords, $options: 'i' } });
             }
         }
-        if (matchOr.length > 0) matchObj["$or"] = matchOr;
+        if (exactOr.length > 0) match["$or"] = exactOr;
     }
 
-    for (let key in match) {
-        /** match 为对象 对象的key为要匹配的字段 val为要精确匹配的数值 */
+    for (let key in exact) {
+        /** exact 为对象 对象的key为要匹配的字段 val为要精确匹配的数值 */
         let docField = getCLfield(CLdoc, key);
-        if (docField.type === ObjectId) return "match 中不传递 ObjectId 的值 需要的话 请放到 includes中"
+        if (docField.type === ObjectId) return "exact 中不传递 ObjectId 的值 需要的话 请放到 includes中"
         if (docField.errMsg) {
             if (IS_STRICT) {
                 return docField.errMsg;
@@ -59,7 +69,7 @@ module.exports = (ctxObj, MToptions) => {
                 continue;
             }
         }
-        matchObj[key] = match[key];
+        match[key] = exact[key];
     }
 
     for (let key in includes) {
@@ -79,14 +89,14 @@ module.exports = (ctxObj, MToptions) => {
                     else return `[filter.includes.${key}] 类型为 ObjectId`;
                 }
             }
-            matchObj[key] = { "$in": val };
+            match[key] = { "$in": val };
         } else {
             if (!isObjectIdAbs(val)) {
                 /** 如果不是 ObjectId 是对象的话 查找 则把对象中的 _id 提取出来 */
                 if (isObject(val) && isObjectIdAbs(val._id)) val = val._id;
                 else return `[filter.includes.${key}] 类型为 ObjectId`;
             }
-            matchObj[key] = val;
+            match[key] = val;
         }
     }
     // for (let key in excludes) {
@@ -106,14 +116,14 @@ module.exports = (ctxObj, MToptions) => {
     //                 else return `[filter.excludes.${key}] 类型为 ObjectId`;
     //             }
     //         }
-    //         matchObj[key] = { "$nin": val };
+    //         match[key] = { "$nin": val };
     //     } else {
     //         if (!isObjectIdAbs(val)) {
     //             /** 如果不是 ObjectId 是对象的话 查找 则把对象中的 _id 提取出来 */
     //             if (isObject(val) && isObjectIdAbs(val._id)) val = val._id;
     //             else return `[filter.excludes.${key}] 类型为 ObjectId`;
     //         }
-    //         matchObj[key] = { "$ne": val };
+    //         match[key] = { "$ne": val };
     //     }
     // }
 
@@ -124,7 +134,7 @@ module.exports = (ctxObj, MToptions) => {
             else continue;
         }
         if (!docField.type) return `[filter.lte 的 key ${key}] 必须为基础类型`;
-        matchObj[key] = { "$lte": lte[key] };
+        match[key] = { "$lte": lte[key] };
     }
 
     for (let key in gte) {
@@ -136,7 +146,7 @@ module.exports = (ctxObj, MToptions) => {
 
         if (!docField.type) return `[filter.gte 的 key ${key}] 必须为基础类型`;
 
-        matchObj[key] = { "$gte": gte[key] };
+        match[key] = { "$gte": gte[key] };
     }
     for (let key in at_before) {
         let docField = getCLfield(CLdoc, key);
@@ -152,7 +162,7 @@ module.exports = (ctxObj, MToptions) => {
         if(!isNaN(at_before[key])) at_before[key] = parseInt(at_before[key]);   // 如果收到的是时间戳
 
         let before = (new Date(at_before[key]).setHours(23, 59, 59, 999));      // 按天算时间
-        matchObj[key] = { "$lte": before };
+        match[key] = { "$lte": before };
     }
     for (let key in at_after) {
         let docField = getCLfield(CLdoc, key);
@@ -168,8 +178,6 @@ module.exports = (ctxObj, MToptions) => {
         if(!isNaN(at_after[key])) at_after[key] = parseInt(at_after[key]);   // 如果收到的是时间戳
 
         let after = (new Date(at_after[key]).setHours(0, 0, 0, 0));     // 按天算时间
-        matchObj[key] = { "$gte": after };
+        match[key] = { "$gte": after };
     }
-
-    ctxObj.reqBody.match = matchObj;
 }
